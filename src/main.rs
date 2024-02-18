e363eca1bca71382d93a107688a0c9f72818289f
Mokarran <9244335+Sphyrna-029@users.noreply.github.com>
2024-02-18 00:23:50 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, HttpRequest, Responder};
+use std::net::IpAddr;
 use serde::Serialize;
 use serde::Deserialize;
-//use serde_json;
+use serde_json;
 use std::{
-    net::SocketAddr,
     sync::Mutex,
     time::SystemTime
 };
 
 
-//Hashmap to store server data
+//Struct to store server data
 #[derive(Serialize, Debug)]
 struct ServerObject {
-    ip: Option<SocketAddr>,
-    port: i32,
-    time: SystemTime,
+    pub ip: IpAddr,
+    pub port: i32,
+    pub time: SystemTime,
+}
+
+
+//Server json 
+#[derive(Serialize, Debug)]
+struct ServerJson {
+    ipport: String
 }
 
 
 //List struct that builds our server vector with a mutex
 #[derive(Serialize)]
 struct ServerList {
-    list: Mutex<Vec<ServerObject>>,
+    pub list: Mutex<Vec<ServerObject>>,
 }
 
 
@@ -42,18 +49,32 @@ async fn hello() -> impl Responder {
 //Endpoint to handle server registration
 #[get("/announce")]
 async fn announce(data: web::Data<ServerList>, servermeta: web::Query<ServerMeta>, req: HttpRequest) -> impl Responder {
-    
     let mut serverlist = data.list.lock().unwrap();
+    let val = req.peer_addr();
 
     let serverinstance = ServerObject {
-        ip: req.peer_addr(),
+        ip: val.expect("Someone connected without an ip!!1").ip(),
         port: servermeta.port,
         time: SystemTime::now(),
 
     };
 
-    serverlist.push(serverinstance);
-    HttpResponse::Ok().body("Something happened")
+    //Check for existing server ip/port in the vector 
+    if serverlist.len() > 0 {
+        for server in serverlist.iter_mut() {
+            if (server.ip == val.expect("").ip()) && (server.port == servermeta.port) {
+                server.ip = val.expect("").ip();
+                server.port = servermeta.port;
+                server.time = SystemTime::now();
+            } else {
+                serverlist.push(serverinstance);
+            }
+        }
+    } else {
+        serverlist.push(serverinstance);
+    }
+
+    HttpResponse::Ok().json("success")
 }
 
 
@@ -62,12 +83,30 @@ async fn announce(data: web::Data<ServerList>, servermeta: web::Query<ServerMeta
 async fn list(data: web::Data<ServerList>) -> impl Responder {
     let mut serverlist = data.list.lock().unwrap();
     
+    //Prune all servers in the list outside the ttk threshold
     //Much faster than .remove() as rust doesnt have to iterate over the vector for each element then shift indexes after removal
     serverlist.retain(|server| SystemTime::now().duration_since(server.time).unwrap().as_secs() <= 60);
-
     println!("{:#?}", serverlist);
 
-    HttpResponse::Ok().body("json")
+    //ToDo: Copy all of our server records to new structs that are more friendly for json formatting then ship
+    if serverlist.len() > 0 {
+        for server in serverlist.iter_mut() {
+            let server_ip = server.ip.to_string();
+            let server_port = server.port.to_string();
+            let server_address = format!("{}:{}", server_ip, server_port);
+            
+            let server_instance = ServerJson {
+                ipport: server_address
+            };
+
+            println!("{:#?}", server_instance);
+        }
+
+    }
+
+    let response_body = serde_json::to_string(&*serverlist).unwrap();
+
+    HttpResponse::Ok().body(response_body)
 }
 
 
